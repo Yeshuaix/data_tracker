2c129b17a41c9d87499e7fdc490d33bd8b485104
夏某人 <Yeshuaix@gmail.com>
2018-12-21 11:12:34 +0800

    Update readData.js

--- mysql_data/readData.js
+++ mysql_data/readData.js
@@ -1,7 +1,5 @@
-
 const db=require("db");
 var mysql = db.open("mysql://root:123456@127.0.0.1:3306/fibos_chain");
-
 
 let readData=function(){
 	//当前所有注册用户数
@@ -12,13 +10,11 @@ let readData=function(){
 	var result2=mysql.execute("select count(id) fo_user_num from actions where authorization='fibosmaster1' ");
 	fo_add=result2[0].fo_user_num;
 
-
 	//进行action操作较多的用户
 	var result5=mysql.execute("select `authorization`,count('authorization') as count_auth from actions group by authorization order by count_auth desc;");
 	var max_action_auth=JSON.parse(result5[0].authorization)[0];
 	var max_action_auth_num=result5[0].count_auth;
 
-	
 	var result4=mysql.execute("select data from actions where `action_name` like '%transfer' ;");
 	let n= mysql.execute("select count(id) as count from actions where `action_name` like '%transfer' ;");
 
@@ -38,7 +34,7 @@ let readData=function(){
 		}
 	}
 	
-	// 兑换EOS排行榜,
+	// 兑换EOS排行榜
 	var exchange_ranking=new Array();
 	var result6=mysql.execute("select data from actions where `action_name` like '%exchange%' ;");
 	let r6_num=mysql.execute("select count(data) as num from actions where action_name like '%exchange%';");
@@ -46,7 +42,6 @@ let readData=function(){
 		exchange_ranking.push(JSON.parse(result6[i].data).quantity.quantity);
 	}
 	
-
 	// FIBOS主网上合约部署的数量(action 种类的数量)
 	var result9=mysql.execute("select count(distinct action_name) as action_num from actions");
 	// console.log("action 的种类数量:"+result9[0].action_num);
@@ -61,22 +56,18 @@ let readData=function(){
 		}	
 	}
 
-
 	data={
-
 		exchange_ranking,       //兑换EOS排行榜
 		transfer_from_ranking,  //充值EOS排行榜
 		transfer_to_ranking,    //兑出EOS排行榜
 		max_action_auth,        //每月操作action最多的用户
-		max_action_auth_num,    //操作次数
+		max_action_auth_num,    //每月操作action最多用户的操作次数
 		register_num,           //注册用户数
 		fo_add,                 //使用钱包注册的用户
 		action_sort_num,        //action种类数量
 		setcode                 //部署合约数量
 	}
-
 	return data;
-
 }
 
 console.log(readData());
